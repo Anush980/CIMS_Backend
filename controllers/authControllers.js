83d0dc7279438c30ba4f4cc37bb8865c7f53296a
Anush Shrestha <anush.stha980@outlook.com>
2025-08-30 00:20:58 +0545

    Added:auth backend with mongoose

---
+++ controllers/authControllers.js
@@ -0,0 +1,59 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const registerUser = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+         const hashedPassword = await bcrypt.hash(password, 10);
+         
+        const newUser = new User({ email, password: hashedPassword });
+        await newUser.save();
+
+        res.status(201).json({ message: "User registered successfully" });
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Internal Server error" });
+    }
+};
+
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const existingUser = await User.findOne({ email });
+        if (!existingUser) {
+            return res.status(400).json({ message: "Invalid email or password" });
+        }
+        const isPasswordValid = await bcrypt.compare(password, existingUser.password);
+        if (!isPasswordValid) {
+            return res.status(400).json({ message: "Invalid email or password" });
+        }
+
+        const token = jwt.sign({
+            id: existingUser._id,
+            email: existingUser.email
+        },
+            process.env.JWT_SECRET,
+            { expiresIn: '7d' });
+
+        res.status(200).json({
+            message: 'Login Successful',
+            user: {
+                id: existingUser._id,
+                email: existingUser.email
+            }, token
+        });
+    }
+
+    catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Internal server error" })
+    }
+}
+module.exports = { registerUser ,loginUser};
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+
+const app = express();
+const port = process.env.PORT||8000;
+
+app.use(express.json());
+app.use(cors());
+
+const authRoutes = require("./routes/authRoutes");
+app.use("/api/auth", authRoutes)
+
+mongoose.connect(
+process.env.MONGODB_URI)
+.then(()=>console.log("Connected to the database"))
+.catch((err)=>console.error("Error connecting to the database",err));
+
+
+
+app.listen(port,()=>{
+console.log(`Server is running on port ${port}`);
+});
