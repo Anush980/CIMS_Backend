28ac31bc3028530c040a1f21cd017555344061e2
Anush Shrestha <anush.stha980@outlook.com>
2025-12-18 06:16:57 +0545

    updated middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -5,16 +5,15 @@ const authMiddleware = (req, res, next) => {
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({ message: "Please login first" });
   }
+
   const token = authHeader.split(" ")[1];
+
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; //this stores user id
+    req.user = decoded; // contains id, email, role, shopName
     next();
   } catch (err) {
-    return res
-      .status(401)
-      .json({ message: "Invalid or expired token. Please login again" });
-
+    return res.status(401).json({ message: "Invalid or expired token" });
   }
 };
 
